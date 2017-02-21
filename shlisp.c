ebccbc923690cd9b613554026552d8a3dc41d891
Davy Peter Braun <hello@davybraun.com>
2017-02-21 09:56:43 +0100

    Rename main file.

---
+++ shlisp.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpc.h"
+
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+// Synthetic readline function for Windows.
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) - 1] = '\0';
+  return cpy;
+}
+
+// Synthetic add_history function for Windows.
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#endif
+
+int main(int argc, char** argv) {
+  // Create some parsers.
+  mpc_parser_t* Number     = mpc_new("number");
+  mpc_parser_t* Operator   = mpc_new("operator");
+  mpc_parser_t* Expression = mpc_new("expression");
+  mpc_parser_t* Shlisp     = mpc_new("shlisp");
+
+  // Define them with the following language.
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "\
+      number     : /-?[0-9]+/;                                 \
+      operator   : '+' | '-' | '*' | '/';                      \
+      expression : <number> |'(' <operator> <expression>+ ')'; \
+      shlisp     : /^/ <operator> <expression>+ /$/;           \
+    ",
+  Number, Operator, Expression, Shlisp);
+
+  puts("Shlisp v0.0.1");
+  puts("Press Ctrl + C to exit REPL\n");
+
+  while (1) {
+    // Get user input from prompt and add to history.
+    char* input = readline("shlisp> ");
+    add_history(input);
+
+    // Attempt to parse user input.
+    // Print abstract syntax tree or error.
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Shlisp, &r)) {
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
+    free(input);
+  }
+
+  // Undefine and delete our parsers.
+  mpc_cleanup(4, Number, Operator, Expression, Shlisp);
+
+  return 0;
+}
